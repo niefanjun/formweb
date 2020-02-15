bb5b3ceef68ae0e75a9c9a41fc616177068ff262
niefanjun <niefanjun@gmail.com>
2020-02-15 13:49:17 +0800

    修复rem问题

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 
 // 连接数据库
-const dbRoute = 'mongodb://xxx:xxx@0.0.0.0:27017/xxx';
+const dbRoute = 'mongodb://node:node@39.98.173.75:27017/testdb';
 mongoose.connect(dbRoute, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
