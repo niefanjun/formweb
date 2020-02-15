c051ad8b016f79f081b87b80775955034d5a960a
niefanjun <niefanjun@gmail.com>
2020-02-15 14:32:14 +0800

    调整message样式

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 
 // 连接数据库
-const dbRoute = 'mongodb://xxx:xxx@0.0.0.0:27017/xxx';
+const dbRoute = 'mongodb://xxx:xxx@xxx:xxx/xxx';
 mongoose.connect(dbRoute, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
